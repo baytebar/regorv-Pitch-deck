828c36a48caacd64e81e292d12bfeb250b38565e
baytebar <baytebar25@gmail.com>
2026-01-14 08:48:25 +0530

    vercel bugs fixed

--- vite.config.js
+++ vite.config.js
@@ -20,13 +20,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     cssCodeSplit: true,
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild',
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'framer-motion', 'lucide-react'],
